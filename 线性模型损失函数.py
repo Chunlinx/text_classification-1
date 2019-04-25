58438b1822f2b08b4ff421d2d61e8a40d6922e3c
baiziyuandyufei <baiziyuandyufei@126.com>
2019-04-25 22:21:09 +0800

    线性模型损失函数

--- 线性模型损失函数.py
+++ 线性模型损失函数.py
@@ -1,10 +1,9 @@
 #coding:utf-8
 import numpy as np
-import math
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+font = FontProperties(fname='/Library/Fonts/Songti.ttc')
 
-plt.rcParams['font.sans-serif'] = ['SimHei']
-plt.rcParams['axes.unicode_minus'] = False
 plt.figure(figsize=(8, 5))
 
 x = np.arange(-10, 10, 0.01)
@@ -20,11 +19,11 @@ y_hinge[y_hinge < 0] = 0
 plt.xlim([-3, 3])
 plt.ylim([0, 4])
 plt.plot(x, logi, 'r-', mec='k', label='Logistic Loss', lw=2)
-plt.plot(x, y_p, 'g-', mec='k', label='0/1 Loss', lw=2)
+plt.plot(x, y_p, 'g-', mec='k', label='Perceptron Loss', lw=2)
 plt.plot(x, y_hinge, 'b-', mec='k', label='Hinge Loss', lw=2)
 plt.grid(True, ls='--')
 plt.legend(loc='upper right')
-plt.title('损失函数')
+plt.title('各种不同的损失函数', fontproperties=font)
 plt.xlabel('yf(x,w)')
 plt.ylabel('loss')
 plt.show()
